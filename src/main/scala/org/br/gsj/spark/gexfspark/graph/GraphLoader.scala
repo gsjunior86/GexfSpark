0668d4ca900d106514e668eb748a5ab4d91861a2
Geraldo <gsjunior@mail.uni-paderborn.de>
2018-02-14 16:52:48 +0100

    getNodes function improval

--- src/main/scala/org/br/gsj/spark/gexfspark/graph/GraphLoader.scala
+++ src/main/scala/org/br/gsj/spark/gexfspark/graph/GraphLoader.scala
@@ -1,12 +1,19 @@
 package org.br.gsj.spark.gexfspark.graph
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.br.gsj.spark.gexfspark.utils.Utils._
 
 object GraphLoader {
   
   def getNodes(spark: SparkSession, path: String) ={
-    
-    
+    var df = spark.sqlContext.read
+      .format("com.databricks.spark.xml")
+      .option("rowTag", "nodes")
+      .load(path)
+    df = df.withColumn("nodes", explode(df("node"))).drop("node")
+    df = df.select(getStructCols("nodes", df): _*)
+    df.show
     
   }
   
--- src/test/scala/org/br/gsj/spark/gexfspark/tests/GraphLoaderTest.scala
+++ src/test/scala/org/br/gsj/spark/gexfspark/tests/GraphLoaderTest.scala
@@ -1,11 +1,17 @@
 package org.br.gsj.spark.gexfspark.tests
 
 import org.apache.spark.sql.SparkSession
+import org.br.gsj.spark.gexfspark.graph.GraphLoader
 
 object GraphLoaderTest {
   def main(args: Array[String]): Unit = {
     
     val spark = SparkSession.builder()
     .appName("GexfSparkTest").master("local[*]").getOrCreate()
+    
+    val path = "/home/gsjunior/Documents/gexf/hierarchy4.gexf"
+    
+    GraphLoader.getNodes(spark, path)
+    
   }
 }
