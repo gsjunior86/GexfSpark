29fb73f5547e8fa12e5655205d93703ed1bfdaf5
Geraldo <gsjunior@mail.uni-paderborn.de>
2018-02-15 17:41:32 +0100

    restructure of the access

--- src/main/scala/org/br/gsj/spark/gexfspark/graph/GraphLoader.scala
+++ src/main/scala/org/br/gsj/spark/gexfspark/graph/GraphLoader.scala
@@ -11,27 +11,12 @@ class GraphLoader (val spark: SparkSession, val path: String) {
   
   
   
-  private var nodes_rdd:RDD[Node] = null
-  private var edges_rdd:RDD[Edge] = null
-  
-  createGraph(spark, path)
+  val nodes :RDD[Node] = createNodes(spark,path)
+  val edges :RDD[Edge] = createEdges(spark,path)
   
   
   
-  private def createGraph(spark: SparkSession, path: String) = {
-      createNodes(spark,path)
-      createEdges(spark,path)
-  }
-  
-  def nodes(): RDD[Node] = {
-    nodes_rdd
-  }
-  
-  def edges(): RDD[Edge] = {
-    edges_rdd
-  }
-  
-  private def createNodes(spark: SparkSession, path: String) = {
+  private def createNodes(spark: SparkSession, path: String): RDD[Node] = {
       var df_nodes = spark.sqlContext.read
       .format("com.databricks.spark.xml")
       .option("rowTag", "nodes")
@@ -39,17 +24,17 @@ class GraphLoader (val spark: SparkSession, val path: String) {
         df_nodes = df_nodes.withColumn("nodes", explode(df_nodes("node"))).drop("node")
         df_nodes = df_nodes.select(getStructCols("nodes", df_nodes): _*)
         df_nodes.show
-        nodes_rdd = df_nodes.rdd.map(f => new Node(f.getAs("_id"),f.getAs("_label")))
+        df_nodes.rdd.map(f => new Node(f.getAs("_id"),f.getAs("_label")))
   }
   
-  private def createEdges(spark: SparkSession, path: String) ={
+  private def createEdges(spark: SparkSession, path: String): RDD[Edge] ={
     var df_edges = spark.sqlContext.read
       .format("com.databricks.spark.xml")
       .option("rowTag", "edges")
       .load(path)
         df_edges = df_edges.withColumn("edges", explode(df_edges("edge"))).drop("edge")
         df_edges = df_edges.select(getStructCols("edges", df_edges): _*)
-        edges_rdd = df_edges.rdd.map(f => new Edge(f.getAs("_source"),f.getAs("_target")))
+        df_edges.rdd.map(f => new Edge(f.getAs("_source"),f.getAs("_target")))
   }
   
   
