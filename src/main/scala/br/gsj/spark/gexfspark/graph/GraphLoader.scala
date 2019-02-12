cc71318d8aaad3cef8bb5f83491eb6ba6e8829ed
gsjunior <gsjunior@localhost>
2019-02-13 00:48:25 +0100

    fixed edges wrong reference

--- src/main/scala/br/gsj/spark/gexfspark/graph/GraphLoader.scala
+++ src/main/scala/br/gsj/spark/gexfspark/graph/GraphLoader.scala
@@ -11,61 +11,55 @@ import org.apache.spark.graphx.VertexId
 import org.apache.spark.graphx.Graph
 import br.gsj.spark.gexfspark.enums.FileType
 
-
 /**
  * Transforms a graph from a filetype to GraphX
- * 
+ *
  * @author Geraldo de Souza Junior
- * 
+ *
  */
 
-class GraphLoader(spark: SparkSession, path: String, fileType : FileType.Value) {
-  
-  
-  
-  var nodes  = createNodes(spark,path)
-  var edges  = createEdges(spark, path)
+class GraphLoader(spark: SparkSession, path: String, fileType: FileType.Value) {
 
-  
-  def loadGraphXGraph(): Graph[String,String] ={
+  var nodes = createNodes(spark, path)
+  var edges = createEdges(spark, path)
+
+  def loadGraphXGraph(): Graph[String, String] = {
+
+    val nodes_graph: RDD[(VertexId, String)] = nodes.map(f => (f.id.toLong, f.label))
     
-       val nodes_graph: RDD[(VertexId, String)] = nodes.zipWithUniqueId().map(f => (f._2, f._1.label))
-       val edges_temp = edges.map(f => (f.source, f.target))
-      
-       val edges_graph = nodes_graph.map(f => f.swap).join(edges_temp).map(f => (f._2._2,f._2))
-       .join(nodes_graph.map(f => f.swap)).map(f => org.apache.spark.graphx.Edge(f._2._1._1.toLong,f._2._2.toLong,""))
-       Graph(nodes_graph,edges_graph)
-        
-  }
+    val edges_graph = edges.map(f =>org.apache.spark.graphx.Edge (f.source.toLong, f.target.toLong,""))
    
+    Graph(nodes_graph, edges_graph)
+
+
+  }
 
   private def createNodes(spark: SparkSession, path: String): RDD[Node] = {
-    if (fileType.equals(FileType.Gexf)){
+    if (fileType.equals(FileType.Gexf)) {
       var df_nodes = spark.sqlContext.read
-      .format("com.databricks.spark.xml")
-      .option("rowTag", "nodes")
-      .load(path)
-        df_nodes = df_nodes.withColumn("nodes", explode(df_nodes("node"))).drop("node")
-        df_nodes = df_nodes.select(getStructCols("nodes", df_nodes): _*)
-        df_nodes.rdd.map(f => new Node(f.getAs("_id").toString,f.getAs("_label").toString))
-    }else{
+        .format("com.databricks.spark.xml")
+        .option("rowTag", "nodes")
+        .load(path)
+      df_nodes = df_nodes.withColumn("nodes", explode(df_nodes("node"))).drop("node")
+      df_nodes = df_nodes.select(getStructCols("nodes", df_nodes): _*)
+      df_nodes.rdd.map(f => new Node(f.getAs("_id").toString, f.getAs("_label").toString))
+    } else {
       null
     }
   }
-  
-  private def createEdges(spark: SparkSession, path: String): RDD[Edge] ={
-    if (fileType.equals(FileType.Gexf)){
+
+  private def createEdges(spark: SparkSession, path: String): RDD[Edge] = {
+    if (fileType.equals(FileType.Gexf)) {
       var df_edges = spark.sqlContext.read
         .format("com.databricks.spark.xml")
         .option("rowTag", "edges")
         .load(path)
-          df_edges = df_edges.withColumn("edges", explode(df_edges("edge"))).drop("edge")
-          df_edges = df_edges.select(getStructCols("edges", df_edges): _*)
-          df_edges.rdd.map(f => new Edge(f.getAs("_source").toString,f.getAs("_target").toString))
-    }else{
+      df_edges = df_edges.withColumn("edges", explode(df_edges("edge"))).drop("edge")
+      df_edges = df_edges.select(getStructCols("edges", df_edges): _*)
+      df_edges.rdd.map(f => new Edge(f.getAs("_source").toString, f.getAs("_target").toString))
+    } else {
       null
     }
   }
-  
-  
+
 }
